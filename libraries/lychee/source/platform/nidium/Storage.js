afd7b035fae96e31804b85a7d3c3fd4b8b649297
Cookie Engineer <cookiengineer@protonmail.ch>
2017-04-12 18:11:06 +0200

    :rocket: lychee.Storage

---
+++ libraries/lychee/source/platform/nidium/Storage.js
@@ -0,0 +1,559 @@
+
+lychee.define('Storage').tags({
+	platform: 'nidium'
+}).includes([
+	'lychee.event.Emitter'
+]).supports(function(lychee, global) {
+
+	if (typeof global.File === 'function') {
+		return true;
+	}
+
+
+	return false;
+
+}).exports(function(lychee, global, attachments) {
+
+	let   _id         = 0;
+	const _Emitter    = lychee.import('lychee.event.Emitter');
+	const _File       = global.File;
+	const _JSON       = {
+		encode: JSON.stringify,
+		decode: JSON.parse
+	};
+	const _PERSISTENT = {
+		data: {},
+		read: function() {
+
+			let url  = lychee.environment.resolve('./lychee.store');
+			let file = new _File(url, { encoding: 'utf8' });
+
+
+			let raw = null;
+			try {
+				file.openSync('r');
+				raw = file.readSync();
+				file.closeSync();
+			} catch (err) {
+				raw = null;
+			}
+
+
+			let buffer = null;
+			try {
+				buffer = JSON.parse(raw);
+			} catch (err) {
+				buffer = null;
+			}
+
+
+			if (buffer !== null) {
+
+				for (let id in buffer) {
+					_PERSISTENT.data[id] = buffer[id];
+				}
+
+
+				return true;
+
+			}
+
+
+			return false;
+
+		},
+		write: function() {
+
+			let buffer = _JSON.encode(_PERSISTENT.data);
+			let url    = lychee.environment.resolve('./lychee.store');
+			let file   = new _File(url, { encoding: 'utf8' });
+
+
+			let result = false;
+			try {
+				file.openSync('w+');
+				result = file.writeSync(buffer);
+				file.closeSync();
+			} catch (err) {
+				result = false;
+			}
+
+
+			return result;
+
+		}
+	};
+	const _TEMPORARY  = {
+		data: {},
+		read: function() {
+
+			if (Object.keys(this.data).length > 0) {
+				return this.data;
+			}
+
+
+			return null;
+
+		},
+		write: function() {
+			return true;
+		}
+	};
+
+
+
+	/*
+	 * FEATURE DETECTION
+	 */
+
+	(function() {
+
+		if (lychee.debug === true) {
+
+			let methods = [];
+
+			if (_PERSISTENT) methods.push('Persistent');
+			if (_TEMPORARY)  methods.push('Temporary');
+
+			if (methods.length === 0) {
+				console.error('lychee.Storage: Supported methods are NONE');
+			} else {
+				console.info('lychee.Storage: Supported methods are ' + methods.join(', '));
+			}
+
+		}
+
+
+		_PERSISTENT.read();
+
+	})();
+
+
+
+	/*
+	 * HELPERS
+	 */
+
+	const _read_storage = function(silent) {
+
+		silent = silent === true;
+
+
+		let id   = this.id;
+		let blob = null;
+
+
+		let type = this.type;
+		if (type === Composite.TYPE.persistent) {
+			blob = _PERSISTENT.data[id] || null;
+		} else if (type === Composite.TYPE.temporary) {
+			blob = _TEMPORARY.data[id]  || null;
+		}
+
+
+		if (blob !== null) {
+
+			if (this.model === null) {
+
+				if (blob['@model'] instanceof Object) {
+					this.model = blob['@model'];
+				}
+
+			}
+
+
+			if (Object.keys(this.__objects).length !== Object.keys(blob['@objects']).length) {
+
+				if (blob['@objects'] instanceof Object) {
+
+					this.__objects = {};
+
+					for (let o in blob['@objects']) {
+						this.__objects[o] = blob['@objects'][o];
+					}
+
+
+					if (silent === false) {
+						this.trigger('sync', [ this.__objects ]);
+					}
+
+
+					return true;
+
+				}
+
+			}
+
+		}
+
+
+		return false;
+
+	};
+
+	const _write_storage = function(silent) {
+
+		silent = silent === true;
+
+
+		let operations = this.__operations;
+		if (operations.length > 0) {
+
+			while (operations.length > 0) {
+
+				let operation = operations.shift();
+				if (operation.type === 'update') {
+
+					if (this.__objects[operation.id] !== operation.object) {
+						this.__objects[operation.id] = operation.object;
+					}
+
+				} else if (operation.type === 'remove') {
+
+					if (this.__objects[operation.id] !== undefined) {
+						delete this.__objects[operation.id];
+					}
+
+				}
+
+			}
+
+
+			let id   = this.id;
+			let blob = {
+				'@model':   this.model,
+				'@objects': this.__objects
+			};
+
+
+			let type = this.type;
+			if (type === Composite.TYPE.persistent) {
+
+				_PERSISTENT.data[id] = blob;
+				_PERSISTENT.write();
+
+			} else if (type === Composite.TYPE.temporary) {
+
+				_TEMPORARY.data[id] = blob;
+
+			}
+
+
+			if (silent === false) {
+				this.trigger('sync', [ this.__objects ]);
+			}
+
+
+			return true;
+
+		}
+
+
+		return false;
+
+	};
+
+
+
+	/*
+	 * IMPLEMENTATION
+	 */
+
+	let Composite = function(data) {
+
+		let settings = Object.assign({}, data);
+
+
+		this.id    = 'lychee-Storage-' + _id++;
+		this.model = {};
+		this.type  = Composite.TYPE.persistent;
+
+
+		this.__objects    = {};
+		this.__operations = [];
+
+
+		this.setId(settings.id);
+		this.setModel(settings.model);
+		this.setType(settings.type);
+
+
+		_Emitter.call(this);
+
+		settings = null;
+
+
+
+		/*
+		 * INITIALIZATION
+		 */
+
+		_read_storage.call(this);
+
+	};
+
+
+	Composite.TYPE = {
+		persistent: 0,
+		temporary:  1
+	};
+
+
+	Composite.prototype = {
+
+		/*
+		 * ENTITY API
+		 */
+
+		sync: function(silent) {
+
+			silent = silent === true;
+
+
+			let result = false;
+
+
+			if (this.__operations.length > 0) {
+				result = _write_storage.call(this, silent);
+			} else {
+				result = _read_storage.call(this, silent);
+			}
+
+
+			return result;
+
+		},
+
+		deserialize: function(blob) {
+
+			if (blob.objects instanceof Object) {
+
+				this.__objects = {};
+
+				for (let o in blob.objects) {
+
+					let object = blob.objects[o];
+
+					if (lychee.interfaceof(this.model, object)) {
+						this.__objects[o] = object;
+					}
+
+				}
+
+			}
+
+		},
+
+		serialize: function() {
+
+			let data = _Emitter.prototype.serialize.call(this);
+			data['constructor'] = 'lychee.Storage';
+
+			let settings = {};
+			let blob     = (data['blob'] || {});
+
+
+			if (this.id.substr(0, 15) !== 'lychee-Storage-') settings.id    = this.id;
+			if (Object.keys(this.model).length !== 0)        settings.model = this.model;
+			if (this.type !== Composite.TYPE.persistent)     settings.type  = this.type;
+
+
+			if (Object.keys(this.__objects).length > 0) {
+
+				blob.objects = {};
+
+				for (let o in this.__objects) {
+
+					let object = this.__objects[o];
+					if (object instanceof Object) {
+						blob.objects[o] = _JSON.decode(_JSON.encode(object));
+					}
+
+				}
+
+			}
+
+
+			data['arguments'][0] = settings;
+			data['blob']         = Object.keys(blob).length > 0 ? blob : null;
+
+
+			return data;
+
+		},
+
+
+
+		/*
+		 * CUSTOM API
+		 */
+
+		create: function() {
+			return lychee.assignunlink({}, this.model);
+		},
+
+		filter: function(callback, scope) {
+
+			callback = callback instanceof Function ? callback : null;
+			scope    = scope !== undefined          ? scope    : this;
+
+
+			let filtered = [];
+
+
+			if (callback !== null) {
+
+				for (let o in this.__objects) {
+
+					let object = this.__objects[o];
+
+					if (callback.call(scope, object, o) === true) {
+						filtered.push(object);
+					}
+
+				}
+
+
+			}
+
+
+			return filtered;
+
+		},
+
+		read: function(id) {
+
+			id = typeof id === 'string' ? id : null;
+
+
+			if (id !== null) {
+
+				let object = this.__objects[id] || null;
+				if (object !== null) {
+					return object;
+				}
+
+			}
+
+
+			return null;
+
+		},
+
+		remove: function(id) {
+
+			id = typeof id === 'string' ? id : null;
+
+
+			if (id !== null) {
+
+				let object = this.__objects[id] || null;
+				if (object !== null) {
+
+					this.__operations.push({
+						type:   'remove',
+						id:     id,
+						object: object
+					});
+
+
+					_write_storage.call(this);
+
+					return true;
+
+				}
+
+			}
+
+
+			return false;
+
+		},
+
+		write: function(id, object) {
+
+			id     = typeof id === 'string'                    ? id     : null;
+			object = lychee.diff(this.model, object) === false ? object : null;
+
+
+			if (id !== null && object !== null) {
+
+				this.__operations.push({
+					type:   'update',
+					id:     id,
+					object: object
+				});
+
+
+				_write_storage.call(this);
+
+				return true;
+
+			}
+
+
+			return false;
+
+		},
+
+		setId: function(id) {
+
+			id = typeof id === 'string' ? id : null;
+
+
+			if (id !== null) {
+
+				this.id = id;
+
+				return true;
+
+			}
+
+
+			return false;
+
+		},
+
+		setModel: function(model) {
+
+			model = model instanceof Object ? model : null;
+
+
+			if (model !== null) {
+
+				this.model = _JSON.decode(_JSON.encode(model));
+
+				return true;
+
+			}
+
+
+			return false;
+
+		},
+
+		setType: function(type) {
+
+			type = lychee.enumof(Composite.TYPE, type) ? type : null;
+
+
+			if (type !== null) {
+
+				this.type = type;
+
+				return true;
+
+			}
+
+
+			return false;
+
+		}
+
+	};
+
+
+	return Composite;
+
+});
+
